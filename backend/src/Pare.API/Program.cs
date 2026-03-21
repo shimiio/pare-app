16fffb166bae4f88c4af3cde7b247280bef76051
Pasa <pavlo1191@gmail.com>
2026-03-21 18:13:10 +0100

    chore: add Swagger UI

--- backend/src/Pare.API/Program.cs
+++ backend/src/Pare.API/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Swagger
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Swagger
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.Run();
