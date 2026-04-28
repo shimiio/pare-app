773f627449eab51340456d64fb0036e5144e731b
Pasa <pavlo1191@gmail.com>
2026-04-29 00:20:23 +0200

    feat: add interceptors in client

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,8 +1,21 @@
 import axios from "axios";
 
-export const defaultUrl = "http://localhost/api";
+export const defaultUrl = "http://localhost:5000/api";
 export const localUrl = "http://192.168.0.150:5000/api";
 
 export const apiClient = axios.create({
   baseURL: localUrl,
 });
+
+apiClient.interceptors.request.use(
+  function (config) {
+    const token = localStorage.getItem("jwtToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  },
+);
