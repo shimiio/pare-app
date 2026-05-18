5fb7a8c9bd42957a6f82e4e30aed11fe2b4caeb0
Pasa <pavlo1191@gmail.com>
2026-05-18 22:25:29 +0200

    feat: add subscription button

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { Plus } from "lucide-react";
 import { useSubscriptions } from "../hooks/useSubscriptions";
 import type { Subscription } from "../types";
 import { getDaysUtil, getLabelColor } from "../utils/dateUtils";
@@ -10,6 +12,7 @@ import {
   Tooltip,
   ResponsiveContainer,
 } from "recharts";
+import CreateSubscriptionModal from "../components/subscriptions/CreateSubscriptionModal";
 
 interface INextPayment {
   name: string;
@@ -20,6 +23,7 @@ interface INextPayment {
 
 export default function Dashboard() {
   const { data, isLoading, isError } = useSubscriptions();
+  const [modal, setModal] = useState<"create" | null>(null);
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>ERROR</div>;
@@ -74,23 +78,31 @@ export default function Dashboard() {
   };
 
   // get most expensive subscription name
-  const mostExpensive = active?.reduce((max, sub) =>
-    getMonthlyAmount(sub) > getMonthlyAmount(max) ? sub : max,
-  );
-
+  const mostExpensive = active?.length
+    ? active.reduce((max, sub) =>
+        getMonthlyAmount(sub) > getMonthlyAmount(max) ? sub : max,
+      )
+    : null;
   // get next payment info from sorted array
-  const getNextPayment = (subscription: Subscription[]): typeof nextPayment => {
-    const nextPayment: INextPayment = {
+  const getNextPayment = (
+    subscription: Subscription[],
+  ): INextPayment | null => {
+    if (!subscription.length || subscription.length === 0) return null;
+
+    return {
       name: subscription[0].name,
       nextBillingDate: subscription[0].nextBillingDate,
       price: subscription[0].price,
       currency: subscription[0].currency,
     };
-
-    return nextPayment;
   };
 
-  const nextPayment = getNextPayment(active ?? []);
+  const nextPayment = getNextPayment(sorted ?? []) ?? {
+    name: "N/A",
+    nextBillingDate: new Date().toISOString(),
+    price: 0,
+    currency: "EUR",
+  };
 
   // chart data
   const months = ["Nov", "Dec", "Jan", "Feb", "Mar", "Apr"];
@@ -103,91 +115,110 @@ export default function Dashboard() {
 
   return (
     <>
-      <h1 className="2xl:text-3xl 2xl:mb-10">Dashboard</h1>
+      <h1 className="font-medium 2xl:text-3xl 2xl:mb-2.5">Dashboard</h1>
 
-      <div className="flex flex-row justify-around mb-10 mx-10">
-        <div className="bg-white/7 rounded-2xl 2xl:py-5 2xl:px-11 2xl:w-60">
-          <div className="text-white/70 2xl:text-xl mb-1.5">Per Month</div>
-          <div className="2xl:text-2xl mb-0.5">
-            {formatCurrency(getMonthlyExpenses(active ?? []), "EUR")}
-          </div>
-          <div className="text-white/60 2xl:text-lg">
-            {active?.length} active
-          </div>
-        </div>
-
-        <div className="bg-white/7 rounded-2xl 2xl:py-5 2xl:px-11 2xl:w-60">
-          <div className="text-white/70 2xl:text-xl 2xl:mb-1.5">Per Year</div>
-          <div className="2xl:text-2xl 2xl:mb-0.5">
-            {formatCurrency(getYearlyExprenses(data ?? []), "EUR")}
-          </div>
-          <div className="text-white/60 2xl:text-lg">
-            Expensive: {mostExpensive?.name ?? "N/A"}
-          </div>
-        </div>
-
-        <div className="bg-white/7 rounded-2xl 2xl:py-5 2xl:px-11 2xl:w-60">
-          <div className="text-white/70 text-xl 2xl:mb-1.5">Next Payment</div>
-          <div className="text-2xl mb-0.5">
-            in {getDaysUtil(nextPayment.nextBillingDate)} days
-          </div>
-          <div className="text-white/60 2xl:text-lg">
-            {nextPayment.name} -{" "}
-            {formatCurrency(nextPayment.price, nextPayment.currency)}
-          </div>
-        </div>
-      </div>
-
-      <h2 className="2xl:text-3xl text-white/80 mb-8">Upcoming payment</h2>
-      <div className="space-y-2 mb-10">
-        {sorted?.slice(0, 3).map((sub) => {
-          const days = getDaysUtil(sub.nextBillingDate);
-          const labelColor = getLabelColor(days);
-
-          return (
-            <div key={sub.id} className="flex flex-col">
-              <button className="flex flex-row justify-between 2xl:p-6 2xl:px-16 mx-20 bg-white/5 rounded-xl">
-                <div className="flex flex-row gap-7">
-                  <img
-                    src={`https://www.google.com/s2/favicons?domain=${getDomain(sub.serviceUrl)}&sz=64`}
-                    width={32}
-                  />
-                  <span className="text-xl">{sub.name}</span>
-                </div>
-                <div className="flex flex-row 2xl:gap-5">
-                  <span
-                    className={`flex rounded-2xl p-1 px-3 font-medium gap-1 ${labelColor}`}
-                  >
-                    <span className="font-medium">{days}</span>days
-                  </span>
-                  <span className="flex items-center">
-                    {formatCurrency(sub.price, sub.currency)}
-                  </span>
-                </div>
-              </button>
+      {!active?.length ? (
+        <button
+          onClick={() => setModal("create")}
+          className="flex cursor-pointer items-center duration-200 opacity-60 hover:opacity-100 rounded-xl 2xl:p-3 2xl:ml-3 2xl:gap-1 2xl:mb-10"
+        >
+          <Plus />
+          <span>Add Subscription</span>
+        </button>
+      ) : (
+        <>
+          <div className="flex flex-row justify-around mt-7.5 mb-10 mx-10">
+            <div className="bg-white/7 rounded-2xl 2xl:py-5 2xl:px-11 2xl:w-60">
+              <div className="text-white/70 2xl:text-xl mb-1.5">Per Month</div>
+              <div className="2xl:text-2xl mb-0.5">
+                {formatCurrency(getMonthlyExpenses(active ?? []), "EUR")}
+              </div>
+              <div className="text-white/60 2xl:text-lg">
+                {active?.length} active
+              </div>
             </div>
-          );
-        })}
-      </div>
 
-      {/* Monthly Expenses */}
-      <h2 className="text-white/80 2xl:text-3xl mb-10">Monthly Expenses</h2>
-      <div className="flex justify-center">
-        <ResponsiveContainer width="90%" height={300}>
-          <BarChart data={chartData}>
-            <XAxis dataKey="month" />
-            <YAxis />
-            <Tooltip
-              contentStyle={{
-                backgroundColor: "rgba(0,0,0,0.8)",
-                border: "1px solid rgba(255,255,255,0.2)",
-              }}
-              labelStyle={{ color: "white" }}
-            />
-            <Bar dataKey="amount" fill="rgba(255,255,255,0.4)" />
-          </BarChart>
-        </ResponsiveContainer>
-      </div>
+            <div className="bg-white/7 rounded-2xl 2xl:py-5 2xl:px-11 2xl:w-60">
+              <div className="text-white/70 2xl:text-xl 2xl:mb-1.5">
+                Per Year
+              </div>
+              <div className="2xl:text-2xl 2xl:mb-0.5">
+                {formatCurrency(getYearlyExprenses(active ?? []), "EUR")}
+              </div>
+              <div className="text-white/60 2xl:text-lg">
+                Expensive: {mostExpensive?.name ?? "N/A"}
+              </div>
+            </div>
+
+            <div className="bg-white/7 rounded-2xl 2xl:py-5 2xl:px-11 2xl:w-60">
+              <div className="text-white/70 text-xl 2xl:mb-1.5">
+                Next Payment
+              </div>
+              <div className="text-2xl mb-0.5">
+                in {getDaysUtil(nextPayment.nextBillingDate)} days
+              </div>
+              <div className="text-white/60 2xl:text-lg">
+                {nextPayment.name} -{" "}
+                {formatCurrency(nextPayment.price, nextPayment.currency)}
+              </div>
+            </div>
+          </div>
+
+          <h2 className="2xl:text-3xl text-white/80 mb-8">Upcoming payment</h2>
+          <div className="space-y-2 mb-10">
+            {sorted?.slice(0, 3).map((sub) => {
+              const days = getDaysUtil(sub.nextBillingDate);
+              const labelColor = getLabelColor(days);
+
+              return (
+                <div key={sub.id} className="flex flex-col">
+                  <button className="flex flex-row justify-between 2xl:p-6 2xl:px-16 mx-20 bg-white/5 rounded-xl">
+                    <div className="flex flex-row gap-7">
+                      <img
+                        src={`https://www.google.com/s2/favicons?domain=${getDomain(sub.serviceUrl)}&sz=64`}
+                        width={32}
+                      />
+                      <span className="text-xl">{sub.name}</span>
+                    </div>
+                    <div className="flex flex-row 2xl:gap-5">
+                      <span
+                        className={`flex rounded-2xl p-1 px-3 font-medium gap-1 ${labelColor}`}
+                      >
+                        <span className="font-medium">{days}</span>days
+                      </span>
+                      <span className="flex items-center">
+                        {formatCurrency(sub.price, sub.currency)}
+                      </span>
+                    </div>
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+
+          {/* Monthly Expenses */}
+          <h2 className="text-white/80 2xl:text-3xl mb-10">Monthly Expenses</h2>
+          <div className="flex justify-center">
+            <ResponsiveContainer width="90%" height={300}>
+              <BarChart data={chartData}>
+                <XAxis dataKey="month" />
+                <YAxis />
+                <Tooltip
+                  contentStyle={{
+                    backgroundColor: "rgba(0,0,0,0.8)",
+                    border: "1px solid rgba(255,255,255,0.2)",
+                  }}
+                  labelStyle={{ color: "white" }}
+                />
+                <Bar dataKey="amount" fill="rgba(255,255,255,0.4)" />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
+        </>
+      )}
+      {modal === "create" && (
+        <CreateSubscriptionModal onClose={() => setModal(null)} />
+      )}
     </>
   );
 }
