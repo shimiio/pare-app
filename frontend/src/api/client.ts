2e0c364e380618adc2d1eb4d94a07e262ba34435
Pasa <pavlo1191@gmail.com>
2026-05-26 18:21:44 +0200

    chore: move API base URL to environment variables

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,11 +1,9 @@
 import axios from "axios";
 import { useAuthStore } from "../store/useAuthStore";
 
-export const defaultUrl = "http://localhost:5000/api";
-export const localUrl = "http://192.168.0.150:5000/api";
-
 export const apiClient = axios.create({
-  baseURL: localUrl,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
 });
 
 // Request interceptor to add JWT token to headers
