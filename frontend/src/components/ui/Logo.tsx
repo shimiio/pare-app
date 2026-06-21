b9b4793e71b3787d154054710181d9d3e7bf1f6a
Pasa <pavlo1191@gmail.com>
2026-06-21 14:53:15 +0200

    feat: add MobilVersionWarning component for mobile width

--- frontend/src/components/ui/Logo.tsx
+++ frontend/src/components/ui/Logo.tsx
@@ -3,20 +3,18 @@ import { Sparkle } from "lucide-react";
 export default function Logo() {
   return (
     <div className="flex items-center justify-center">
-      <svg className="absolute w-0 h-0" width="0" height="0">
-        <linearGradient
-          id="my-lucide-gradient"
-          x1="0"
-          y1="0"
-          x2="24"
-          y2="24"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop offset="0%" stopColor="#ec4899" /> {/* Tailwind pink-500 */}
-          <stop offset="50%" stopColor="#8b5cf6" /> {/* Tailwind violet-500 */}
-          <stop offset="100%" stopColor="#3b82f6" /> {/* Tailwind blue-500 */}
-        </linearGradient>
-      </svg>
+      <linearGradient
+        id="my-lucide-gradient"
+        x1="0"
+        y1="0"
+        x2="24"
+        y2="24"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0%" stopColor="#ec4899" /> {/* Tailwind pink-500 */}
+        <stop offset="50%" stopColor="#8b5cf6" /> {/* Tailwind violet-500 */}
+        <stop offset="100%" stopColor="#3b82f6" /> {/* Tailwind blue-500 */}
+      </linearGradient>
 
       <Sparkle className="h-5 w-5 stroke-[url(#my-lucide-gradient)] drop-shadow-lg" />
     </div>
